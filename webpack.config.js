bc3fa35bca8a7fdd99ddc42aeef9ce2908740954
Jeffrey Salinas <jeff.salinas.music@gmail.com>
2019-10-13 16:53:08 -0500

    Reconfigured scripts to have webpack production and webpack development options

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,22 @@ module.exports = {
         }
       }
     ]
+  },
+  devServer: {
+    // Display only errors to reduce the amount of output.
+    stats: "errors-only",
+    contentBase: path.join(__dirname, 'dist'),
+
+    // Parse host and port from env to allow customization.
+    //
+    // If you use Docker, Vagrant or Cloud9, set
+    // host: "0.0.0.0";
+    //
+    // 0.0.0.0 is available to all network devices
+    // unlike default `localhost`.
+    host: process.env.HOST, // Defaults to `localhost`
+    port: process.env.PORT, // Defaults to 8080
+    open: true, // Open the page in browser
   }
 };
 
